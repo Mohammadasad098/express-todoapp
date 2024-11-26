e9df9d2a5542ce980b8ef151892fc598be2f9534
Mohammadasad098 <mohammadasadiam64@gmail.com>
2024-11-26 21:16:35 +0500

    todoapp completed

--- src/controllers/todos.controllers.js
+++ src/controllers/todos.controllers.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import Todos from "../models/todos.models.js";
 
-// add todo
 
 const addTodo = (req, res) => {
   const { title, description } = req.body;
@@ -22,4 +21,81 @@ const addTodo = (req, res) => {
   });
 };
 
-export { addTodo };
+
+
+const getAllTodos = async (req, res) => {
+  const todos = await Todos.find({});
+  res.status(200).json({
+    todos: todos,
+  });
+};
+
+const getTodoWithId = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Not valid Id" });
+  }
+
+  const todo = await Todos.findById(id);
+  if (!todo) {
+    res.status(404).json({
+      message: "no todo found!",
+    });
+    return;
+  }
+
+  res.status(200).json(todo);
+};
+
+
+
+const deleteTodo = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Not valid id" });
+  }
+
+  const todo = await Todos.findOneAndDelete({ _id: id });
+
+  if (!todo) {
+    return res.status(404).json({ error: "No Todo found" });
+  }
+  res.status(200).json({
+    message: "todo deleted successfully",
+    todo,
+  });
+};
+
+
+
+
+const editTodo = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, completed } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid ID" });
+  }
+  try {
+    const updatedTodo = await Todos.findOneAndUpdate(
+      { _id: id },
+      { title, description, completed },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedTodo) {
+      return res.status(404).json({ error: "No Todo found" });
+    }
+
+    res.status(200).json({
+      message: "Todo updated successfully",
+      todo: updatedTodo,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong", details: error.message });
+  }
+};
+
+export { addTodo , getAllTodos , getTodoWithId , deleteTodo , editTodo };
--- src/routes/todos.routes.js
+++ src/routes/todos.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { addTodo } from "../controllers/todos.controllers.js";
+import { addTodo , getAllTodos , getTodoWithId , deleteTodo , editTodo } from "../controllers/todos.controllers.js";
 
 const router = express.Router();
 
 router.post("/todo", addTodo);
+router.get("/allTodos", getAllTodos);
+router.get("/todo/:id", getTodoWithId);
+router.delete("/todo/:id", deleteTodo);
+router.put("/todo/:id", editTodo);
+
 
 export default router;
